cc04a7a547d059cc0a326acc922bc987c6214f3a
carlosdaniel <danieljimenez88m@gmail.com>
2025-10-07 23:21:55 -0500

    rebuilding site Tue Oct  7 23:21:55 -05 2025

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,24 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+# Build the project with Hugo
+hugo
 
-# Go To Public folder
-cd public
-# Add changes to git.
-git add .
+# Copy generated files from public/ to root
+echo -e "\033[0;34mCopying files from public/ to root...\033[0m"
+cp -r public/* .
 
-# Commit changes.
+# Add all changes to git
+git add -A
+
+# Commit changes
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+# Push to GitHub
 git push origin master
 
-# Come Back up to the Project Root
-cd ..
+echo -e "\033[0;32mDone! Your changes will be live in 2-5 minutes.\033[0m"
